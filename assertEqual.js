2044867f0d870dcc0d7f4bd845b96efffd064c6e
juntaodev <juntaoliudev@gmail.com>
2022-11-14 16:49:21 -0500

    create a bug detector using assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,8 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
