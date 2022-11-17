75a3ff868770ebaf98152b97160ee3ab221f329c
juntaodev <juntaoliudev@gmail.com>
2022-11-16 21:16:26 -0500

    refactor

--- countLetters.js
+++ countLetters.js
@@ -1,22 +1,17 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const countLetters = function(sentence) {
-  let result = {};
+  let results = {};
   for (let char of sentence) {
     if (char !== " ") {
-      if (result[char]) {
-        result[char] += 1;
+      if (results[char]) {
+        results[char] += 1;
       } else {
-        result[char] = 1;
+        results[char] = 1;
       }
     }
   }
-  return result;
+  return results;
 };
 
-// console.log(countLetters("lighthouse in the house"));
 // TEST CODE
 console.log(countLetters("lighthouse in the house"));
 
