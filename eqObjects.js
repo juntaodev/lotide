8446d047d76e4739b7cf01a3194c17cdba41b350
juntaodev <juntaoliudev@gmail.com>
2022-11-22 11:12:18 -0500

    refactor eqObjects function

--- eqObjects.js
+++ eqObjects.js
@@ -1,18 +1,6 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`👍👍👍Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
-
 // FUNCTION IMPLEMENTATION
+const eqArrays = require('./eqArrays');
+
 const eqObjects = function(object1, object2) {
   let keys1 = Object.keys(object1);
   let keys2 = Object.keys(object2);
@@ -31,24 +19,4 @@ const eqObjects = function(object1, object2) {
   return result;
 };
 
-// TEST CODE
-// TEST PRIMITIVES
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-eqObjects(ab, ba); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-eqObjects(ab, abc); // => false
-
-assertEqual(eqObjects(ab, ba), true);
-assertEqual(eqObjects(ab, abc), false);
-// TEST ARRAYS
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-eqObjects(cd, dc); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-eqObjects(cd, cd2); // => false
-
-assertEqual(eqObjects(cd, dc), true);
-assertEqual(eqObjects(cd, cd2), false);
+module.exports = eqObjects;
