6c515bc08ca6123b1a5f36744b6263fa445fd0a8
juntaodev <juntaoliudev@gmail.com>
2022-11-22 11:41:50 -0500

    refactor takeUntil function

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,4 @@
+// FUNCTION IMPLEMENTATION
 const takeUntil = function(array, callback) {
   const results = [];
   for (let element of array) {
@@ -8,13 +9,4 @@ const takeUntil = function(array, callback) {
   return results;
 };
 
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+module.exports = takeUntil;
