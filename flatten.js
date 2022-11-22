7ef12e7ecfd0569b39ca1a0b2b61cd5e50fb715b
juntaodev <juntaoliudev@gmail.com>
2022-11-22 11:44:01 -0500

    refactor flatten function

--- flatten.js
+++ flatten.js
@@ -1,17 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  eqArrays(arr1, arr2) ? console.log(`👍👍👍Assertion Passed: ${arr1} === ${arr2}`) : console.log(`❌❌❌Assertion Failed: ${arr1} !== ${arr2}`);
-};
-
+// FUNCTION IMPLEMENTATION
 const flatten = function(array) {
   let results = [];
   for (let element of array) {
@@ -28,7 +15,4 @@ const flatten = function(array) {
   return results;
 };
 
-flatten([1, 2, [3, 4], 5, [6]]);// => [1, 2, 3, 4, 5, 6]
-
-
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+module.exports = flatten;
