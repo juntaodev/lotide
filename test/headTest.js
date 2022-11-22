d02cf830caaa89a8c78108254bf9c587b737baaa
juntaodev <juntaoliudev@gmail.com>
2022-11-21 21:51:40 -0500

    refactor headTest with mocha w/ chai

---
+++ test/headTest.js
@@ -0,0 +1,11 @@
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
